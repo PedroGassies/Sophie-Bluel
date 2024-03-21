3ebe03531f049bde7ff987e1f98ef617ba24c27f
PedroGassies <148749756+PedroGassies@users.noreply.github.com>
2024-03-21 18:41:24 +0100

    requête login casi complète

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,7 +3,7 @@ let token = window.localStorage.getItem('token');
 
 if(token === null){
     //Collect token from API
-const reponse = await fetch("http://localhost:8081/pieces");
+const reponse = await fetch("http://localhost:5678/api/users/login/");
 token= await reponse.json();
 
  //Transforming token into JSON
@@ -15,23 +15,24 @@ token = json.parse(token);
 }
 /****************** LOG IN  ************************/
 export function connect() {
-    const formulaireLogIn = document.querySelector(".connect");
-    formulaireLogIn.addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Création de l’objet du nouvel avis.
-    const logIn = {
-        utilisateur: event.target.querySelector("[name=email").value,
-        commentaire: event.target.querySelector("[name=password]").value,
-    };
-    // Création de la charge utile au format JSON
-    const chargeUtile = JSON.stringify(logIn);
-    // Appel de la fonction fetch avec toutes les informations nécessaires
-    fetch("http://localhost:5678/api/users/login/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: chargeUtile
-    });
-    });
-    
+    const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "email": "sophie.bluel@test.tld",
+  "password": "S0phie"
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("http://localhost:5678/api/users/login", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
  };
 
