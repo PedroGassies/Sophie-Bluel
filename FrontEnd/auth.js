51f16a1da8e598c01e8437321686c9065058bf57
gassies pedro <pedrogassies@gmail.com>
2024-03-26 14:02:48 +0100

    Avancement page editeur

--- FrontEnd/auth.js
+++ FrontEnd/auth.js
@@ -3,9 +3,10 @@ export function ifToken(){
     if(localStorage.getItem('token')){
       document.getElementById('modeEditeur').style.visibility = "visible";
       document.getElementById('log').innerHTML = '<a href="login.html" id="logout">logout';
-      btnProjets.style.visibility="hidden";
-      btnObjets.style.visibility="hidden";
-      btnAppartements.style.visibility="hidden";
-      btnHotels.style.visibility="hidden";
-    }
+    document.querySelector(".btnProjects").style.visibility = "hidden";
+    document.querySelector(".btnObjects").style.visibility = "hidden";
+    document.querySelector(".btnAppartments").style.visibility = "hidden";
+    document.querySelector(".btnHostels").style.visibility = "hidden";
+    
   }
+}
