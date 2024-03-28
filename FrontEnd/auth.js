c14cab1e87167872f93e8b44a915c4aecef313c2
PedroGassies <148749756+PedroGassies@users.noreply.github.com>
2024-03-28 18:45:02 +0100

    Login fixed

--- FrontEnd/auth.js
+++ FrontEnd/auth.js
@@ -2,7 +2,7 @@
 export function ifToken(){
     if(localStorage.getItem('token')){
       document.getElementById('modeEditeur').style.visibility = "visible";
-      document.getElementById('log').innerHTML = '<a href="login.html" id="logout">logout';
+      document.getElementById('log').innerHTML = '<a href="login.html" id="log">logout';
     document.querySelector(".btnProjects").style.visibility = "hidden";
     document.querySelector(".btnObjects").style.visibility = "hidden";
     document.querySelector(".btnAppartments").style.visibility = "hidden";
