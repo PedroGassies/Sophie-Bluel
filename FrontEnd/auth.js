fce7fe68f0782fa37cc81762849b5b1adb84fea6
gassies pedro <pedrogassies@gmail.com>
2024-03-29 12:54:27 +0100

    css editor finished

--- FrontEnd/auth.js
+++ FrontEnd/auth.js
@@ -1,7 +1,9 @@
 
 export function ifToken(){
     if(localStorage.getItem('token')){
-      document.getElementById('modeEditeur').style.visibility = "visible";
+      document.getElementById('headEdit').style.visibility = "visible";
+      document.querySelector('.js-modal').style.visibility = "visible";
+      document.querySelector('.index').style.margin='15px';
       document.getElementById('log').innerHTML = '<a href="login.html" id="log">logout';
     document.querySelector(".btnProjects").style.visibility = "hidden";
     document.querySelector(".btnObjects").style.visibility = "hidden";
